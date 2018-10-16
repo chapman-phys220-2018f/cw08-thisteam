7fe7e7fabfc229718cc37e0ed7165aa797a5de81
Justin Dressel <dressel@chapman.edu>
2018-10-16 07:43:45 +0000

    2018 import

---
+++ test_sinesum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""sinesum.py Test Module
+
+Verify implementation of the Fourier sine series using numpy arrays.
+"""
+
+import sinesum
+
+def test_dummy():
+    """Dummy test that always fails. Please replace with actual tests."""
+    assert False, "This is a dummy test"
