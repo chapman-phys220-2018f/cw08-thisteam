7d1fc6f115005a1061a4619812f724fc1cfa2406
Alley Busick <busick@chapman.edu>
2018-11-06 22:25:06 +0000

    Test file

--- test_sinesum.py
+++ test_sinesum.py
@@ -6,7 +6,12 @@ Verify implementation of the Fourier sine series using numpy arrays.
 """
 
 import sinesum
+import math
 
-def test_dummy():
-    """Dummy test that always fails. Please replace with actual tests."""
-    assert False, "This is a dummy test"
+def test_s():
+    assert math.isclose((sinesum.s(1/(2*math.pi)),1/(4/math.pi)), math.sin(1))
+
+def test_f():
+    assert sinesum.f(1) == 1
+    assert sinesum.f(-1) == -1
+    assert sinesume.f(0) == 0
